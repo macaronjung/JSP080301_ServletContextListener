64c508679bf37257639bae6e8ca8ba17c8d7f03a
Java <Java@DESKTOP-04KTJS7>
2020-01-26 00:31:53 +0900

    Add @WebListener

--- src/com/javalec/ex/ServletCL.java
+++ src/com/javalec/ex/ServletCL.java
@@ -2,7 +2,10 @@ package com.javalec.ex;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
+//web.xml 미사용 -> 리스너 클래스 제작으로 리스너 호출
+@WebListener
 public class ServletCL implements ServletContextListener {
 
 	//웹어플리케이션 종료시 구동
